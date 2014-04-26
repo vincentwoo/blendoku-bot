321c595c32b8aca4987b08a47cf226a91f0aba56
Vincent Woo <me@vincentwoo.com>
2014-04-26 03:51:32 -0700

    consolidating

--- blendoku.coffee
+++ blendoku.coffee
@@ -3,30 +3,69 @@ God help ye, all who enter. We write better code than this, generally.
 Checkout the github for the original coffeescript source.
 ###
 
+_.mixin eachSlice: (obj, size, iterator, context) ->
+  i = 0
+  l = obj.length
+
+  while i < l
+    iterator.call context, obj.slice(i, i + size), i, obj
+    i += size
+
 class Blendoku
 
-  constructor: ({@rows, @cols, @cells, @colors}) ->
-    @grid = [] # grid[i][j] is the ith row, jth column from the bottom left to upper right
-    _.times @rows, => @grid.push []
-    @grid = _.map @grid, () =>
-      ret = []
-      _.times @cols, -> ret.push null
-      ret
+  @TEMPLATE: """
+    <div class="blendoku">
+
+      <table class="palette">
+        <% _.eachSlice(palette, 7, function(row) { %>
+          <tr>
+            <% _.each(row, function(color) { %>
+              <td style="background: #<%= color %>"></td>
+            <% }); %>
+          </tr>
+        <% }); %>
+      </table>
+
+      <table>
+        <% _.each(grid.slice().reverse(), function(row) { %>
+          <tr>
+            <% _.each(row, function(cell) { %>
+              <% if (cell) { %>
+                <td style="
+                  background: #<%= cell.color %>;
+                  <%= cell.color ? 'border: 0' : '' %>
+                ">
+                  <% if (cell.locked) { %>
+                    <span class="fui-lock"></span>
+                  <% } %>
+                </td>
+              <% } else { %>
+                <td style="border: 0"></td>
+              <% } %>
+            <% }); %>
+          </tr>
+        <% }); %>
+      </table>
+
+    </div>
+  """
+
+  constructor: ({@rows, @cols, @cells, @palette}) ->
+    # grid[i][j] is the ith row, jth column from the bottom left to upper right
+    @grid = _.times @rows, =>
+      _.times @cols, -> null
+
     for cell in @cells
-      @grid[cell[0]][cell[1]] = cell[2] || false
-    console.log 'blendoku instantiated with grid:'
-    console.log @grid
+      @grid[cell[0]][cell[1]] = if cell[2]
+        color: cell[2]
+        locked: true
+      else
+        color: null
+        locked: false
 
   render: ->
-    el = $('<table/>')
-    for row in @grid
-      rowEl = $('<tr/>')
-      for cell in row
-        cellEl = $("<td style=\"background: ##{cell}\"></td>")
-        cellEl.css('border', 0) if cell == null || typeof cell == 'string'
-        rowEl.append cellEl
-      el.prepend rowEl
-    el
+    console.log @grid
+    $(_.template(@constructor.TEMPLATE, {@grid, @palette}))
 
 example1 = new Blendoku
   rows: 4
@@ -49,31 +88,33 @@ example1 = new Blendoku
     [2, 4]
     [3, 3]
   ]
-  colors: []
+  palette: ['8c9e42', 'c5be84', '9c9e84', '8c926b', 'a4aa63', 'b5a6a4', '5a6973', '8c86a4', '425952', '73758c', '103d21', '73864a', '294d3a', 'd6db5b']
 
-$('#example-1').append example1.render()
+el = example1.render()
 
-example2 = new Blendoku
-  rows: 4
-  cols: 7
-  cells: [
-    [0, 0, 'a492c5']
-    [0, 1, '8c86a4']
-    [0, 2, '73758c']
-    [0, 3, '5a6973']
-    [0, 4, '425952']
-    [0, 5, '294d3a']
-    [0, 6, '103d21']
-    [1, 1, 'b5a6a4']
-    [1, 2, '9c9e84']
-    [1, 3, '8c926b']
-    [1, 4, '73864a']
-    [1, 5, '4a6931']
-    [2, 2, 'c5be84']
-    [2, 3, 'a4aa63']
-    [2, 4, '8c9e42']
-    [3, 3, 'd6db5b']
-  ]
-  colors: []
+$('#example-1').prepend el
 
-$('#example-2').append example2.render()
+# setInterval ->
+#   replace = example1.render()
+#   el.replaceWith replace
+#   el = replace
+# , 200
+
+###
+[0, 0, 'a492c5']
+[0, 1, '8c86a4']
+[0, 2, '73758c']
+[0, 3, '5a6973']
+[0, 4, '425952']
+[0, 5, '294d3a']
+[0, 6, '103d21']
+[1, 1, 'b5a6a4']
+[1, 2, '9c9e84']
+[1, 3, '8c926b']
+[1, 4, '73864a']
+[1, 5, '4a6931']
+[2, 2, 'c5be84']
+[2, 3, 'a4aa63']
+[2, 4, '8c9e42']
+[3, 3, 'd6db5b']
+###
--- javascripts/blendoku.js
+++ javascripts/blendoku.js
@@ -2,51 +2,54 @@
 God help ye, all who enter. We write better code than this, generally.
 Checkout the github for the original coffeescript source.
 */
-var Blendoku, example1, example2;
+var Blendoku, el, example1;
+
+_.mixin({
+  eachSlice: function(obj, size, iterator, context) {
+    var i, l, _results;
+    i = 0;
+    l = obj.length;
+    _results = [];
+    while (i < l) {
+      iterator.call(context, obj.slice(i, i + size), i, obj);
+      _results.push(i += size);
+    }
+    return _results;
+  }
+});
 
 Blendoku = (function() {
+
+  Blendoku.TEMPLATE = "<div class=\"blendoku\">\n\n  <table class=\"palette\">\n    <% _.eachSlice(palette, 7, function(row) { %>\n      <tr>\n        <% _.each(row, function(color) { %>\n          <td style=\"background: #<%= color %>\"></td>\n        <% }); %>\n      </tr>\n    <% }); %>\n  </table>\n\n  <table>\n    <% _.each(grid.slice().reverse(), function(row) { %>\n      <tr>\n        <% _.each(row, function(cell) { %>\n          <% if (cell) { %>\n            <td style=\"\n              background: #<%= cell.color %>;\n              <%= cell.color ? 'border: 0' : '' %>\n            \">\n              <% if (cell.locked) { %>\n                <span class=\"fui-lock\"></span>\n              <% } %>\n            </td>\n          <% } else { %>\n            <td style=\"border: 0\"></td>\n          <% } %>\n        <% }); %>\n      </tr>\n    <% }); %>\n  </table>\n\n</div>";
 
   function Blendoku(_arg) {
     var cell, _i, _len, _ref,
       _this = this;
-    this.rows = _arg.rows, this.cols = _arg.cols, this.cells = _arg.cells, this.colors = _arg.colors;
-    this.grid = [];
-    _.times(this.rows, function() {
-      return _this.grid.push([]);
-    });
-    this.grid = _.map(this.grid, function() {
-      var ret;
-      ret = [];
-      _.times(_this.cols, function() {
-        return ret.push(null);
+    this.rows = _arg.rows, this.cols = _arg.cols, this.cells = _arg.cells, this.palette = _arg.palette;
+    this.grid = _.times(this.rows, function() {
+      return _.times(_this.cols, function() {
+        return null;
       });
-      return ret;
     });
     _ref = this.cells;
     for (_i = 0, _len = _ref.length; _i < _len; _i++) {
       cell = _ref[_i];
-      this.grid[cell[0]][cell[1]] = cell[2] || false;
+      this.grid[cell[0]][cell[1]] = cell[2] ? {
+        color: cell[2],
+        locked: true
+      } : {
+        color: null,
+        locked: false
+      };
     }
-    console.log('blendoku instantiated with grid:');
-    console.log(this.grid);
   }
 
   Blendoku.prototype.render = function() {
-    var cell, cellEl, el, row, rowEl, _i, _j, _len, _len2, _ref;
-    el = $('<table/>');
-    _ref = this.grid;
-    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-      row = _ref[_i];
-      rowEl = $('<tr/>');
-      for (_j = 0, _len2 = row.length; _j < _len2; _j++) {
-        cell = row[_j];
-        cellEl = $("<td style=\"background: #" + cell + "\"></td>");
-        if (cell === null || typeof cell === 'string') cellEl.css('border', 0);
-        rowEl.append(cellEl);
-      }
-      el.prepend(rowEl);
-    }
-    return el;
+    console.log(this.grid);
+    return $(_.template(this.constructor.TEMPLATE, {
+      grid: this.grid,
+      palette: this.palette
+    }));
   };
 
   return Blendoku;
@@ -57,16 +60,28 @@ example1 = new Blendoku({
   rows: 4,
   cols: 7,
   cells: [[0, 0, 'a492c5'], [0, 1], [0, 2], [0, 3], [0, 4], [0, 5], [0, 6], [1, 1], [1, 2], [1, 3], [1, 4], [1, 5, '4a6931'], [2, 2], [2, 3], [2, 4], [3, 3]],
-  colors: []
+  palette: ['8c9e42', 'c5be84', '9c9e84', '8c926b', 'a4aa63', 'b5a6a4', '5a6973', '8c86a4', '425952', '73758c', '103d21', '73864a', '294d3a', 'd6db5b']
 });
 
-$('#example-1').append(example1.render());
+el = example1.render();
 
-example2 = new Blendoku({
-  rows: 4,
-  cols: 7,
-  cells: [[0, 0, 'a492c5'], [0, 1, '8c86a4'], [0, 2, '73758c'], [0, 3, '5a6973'], [0, 4, '425952'], [0, 5, '294d3a'], [0, 6, '103d21'], [1, 1, 'b5a6a4'], [1, 2, '9c9e84'], [1, 3, '8c926b'], [1, 4, '73864a'], [1, 5, '4a6931'], [2, 2, 'c5be84'], [2, 3, 'a4aa63'], [2, 4, '8c9e42'], [3, 3, 'd6db5b']],
-  colors: []
-});
+$('#example-1').prepend(el);
 
-$('#example-2').append(example2.render());
+/*
+[0, 0, 'a492c5']
+[0, 1, '8c86a4']
+[0, 2, '73758c']
+[0, 3, '5a6973']
+[0, 4, '425952']
+[0, 5, '294d3a']
+[0, 6, '103d21']
+[1, 1, 'b5a6a4']
+[1, 2, '9c9e84']
+[1, 3, '8c926b']
+[1, 4, '73864a']
+[1, 5, '4a6931']
+[2, 2, 'c5be84']
+[2, 3, 'a4aa63']
+[2, 4, '8c9e42']
+[3, 3, 'd6db5b']
+*/
